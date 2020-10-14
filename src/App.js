f9bbfc045a828dd90500bc89c09a14bf242ee62e
kopolito <dev@kopol.xyz>
2020-10-14 11:59:09 +0200

    react_init : basic project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div id="myBooks" className="App">
+      <h1 className="title">Poch'Lib</h1>
+      <h2 className="h2">Nouveau Livre</h2>
+      <hr />
+      <h2>Ma poch'liste</h2>
+    </div>
+  );
+}
+
+export default App;

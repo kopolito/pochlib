f3f02ac8f889379cf0cbf46c9ae0ec7578f502c0
kopolito <dev@kopol.xyz>
2020-11-03 11:10:26 +0100

    added main App logic

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
+import App from './App';
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Poch'Lib/i);
   expect(linkElement).toBeInTheDocument();
 });
